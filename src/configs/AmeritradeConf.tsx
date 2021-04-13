ed28877e074f70832936216f5e1f0b71feec1506
Alex Lee <alexlee500@gmail.com>
2021-04-13 01:48:10 -0700

    gitignore update

--- src/configs/AmeritradeConf.tsx
+++
@@ -1,5 +0,0 @@
-export default {
-    clientId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-    redirectUrl: 'https://reactTestApp/oauth',
-    tokenEndpoint: 'https://api.tdameritrade.com/v1/oauth2/token'
-}
