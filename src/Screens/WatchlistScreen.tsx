00d53f15e8e643cc81b63773e086c9d297057cc0
Alex Lee <alexlee500@gmail.com>
2021-05-01 23:12:58 -0700

    basic watchlist prices

--- src/Screens/WatchlistScreen.tsx
+++ src/Screens/WatchlistScreen.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import  {Text, View, Button } from 'react-native';
+import  {Text, View, Button, FlatList, SafeAreaView } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import { useDispatch, useSelector } from 'react-redux';
-
+import { List, ListItem } from "react-native-elements"
 
 import { selectWatchlist } from '../Redux/features/tdaSlice';
 
@@ -11,16 +11,11 @@ import { quoteSelector } from '../Redux/features/quoteSlice';
 export default function WatchlistScreen() {
     const [selectedWatchlist, setSelectedWatchlist] = useState();
 
-    const allQuotes = useSelector(quoteSelector.selectAll);
-
-    console.log(`selectAll ${JSON.stringify(allQuotes)}`)
-
     const allEntities = useSelector(quoteSelector.selectEntities);
-    console.log(`selectEnts ${JSON.stringify(allEntities)}`)
 
-
-    console.log(`${JSON.stringify(allEntities.AAPL["1"])}`)
-
+    useEffect(() => {
+        console.log(`selectEnts ${JSON.stringify(allEntities)}`)
+    }, [allEntities])
 
     const watchlists:Watchlists = useSelector( selectWatchlist )
     let myLists = watchlists.map((myValue, myIndex) => {
@@ -28,7 +23,10 @@ export default function WatchlistScreen() {
             <Picker.Item label={myValue.name} value={myIndex} key={myIndex}/>
         )
     })
+
+
     return(
+        <SafeAreaView>
         <View>
             <Picker
                 selectedValue={selectedWatchlist}
@@ -38,8 +36,17 @@ export default function WatchlistScreen() {
                 >
                 {myLists}
             </Picker>
-            <Text>{JSON.stringify(watchlists[selectedWatchlist])}</Text>
+            <FlatList
+                data={watchlists[selectedWatchlist]?.watchlistItems}
+                keyExtractor={item => item.instrument.symbol}
+                renderItem={({item}) => (
+                    <Text>{item.instrument.symbol} {(allEntities?.[item.instrument.symbol]?.['1'] || '-')}</Text>
+
+                )}
+            />
+            
             
         </View>
+        </SafeAreaView>
     )
 }
