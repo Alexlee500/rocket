703cc3c88b4735fb8bb5d5859714f4a877c2dbc4
Alex Lee <alexlee500@gmail.com>
2021-05-07 10:03:56 -0700

    cleaned app nav

--- src/Navigation/AppTabNavigator.tsx
+++ src/Navigation/AppTabNavigator.tsx
@@ -1,7 +1,5 @@
-import React, { useEffect } from 'react';
-import  {Text, View, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
+
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 import AccountScreen from '../Screens/AccountScreen';
