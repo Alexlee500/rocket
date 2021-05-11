0222323a171f6e0aa4be826a173d9d43b7e5e3ce
Alex Lee <alexlee500@gmail.com>
2021-05-11 02:36:25 -0700

    Update AccountScreen.tsx

--- src/Screens/AccountScreen.tsx
+++ src/Screens/AccountScreen.tsx
@@ -55,7 +55,6 @@ export default function AccountScreen() {
             <DataTable >
                 <DataTable.Header style={{backgroundColor:Colors.MainDark}}>
                     <DataTable.Title>Symbol</DataTable.Title>
-                    <DataTable.Title numeric>Qty</DataTable.Title>
                     <DataTable.Title numeric>Mark</DataTable.Title>
                     <DataTable.Title numeric>P&L</DataTable.Title>
                 </DataTable.Header>
@@ -66,22 +65,19 @@ export default function AccountScreen() {
                     mainRow={                    
                     <DataTable.Row key={0} >
                     <DataTable.MultiRowCell mainText="GME" subText="+100000" subDirection={1}/>
-                    <DataTable.MultiRowCell numeric mainText="GME" subText="+10" subDirection={1}/>
-                    <DataTable.MultiRowCell numeric mainText="GME" subText="+10" subDirection={1}/>
-                    <DataTable.MultiRowCell numeric mainText="$10000" subText="13" subDirection={1}/>
+                    <DataTable.MultiRowCell numeric mainText="$150.69" subText="+12.2%" subDirection={1}/>
+                    <DataTable.MultiRowCell numeric mainText="$10000" mainDirection={1} subText="+34.2%" subDirection={1}/>
                     </DataTable.Row>
                 }>
                     <DataTable.Row key={0.1}>
                         <DataTable.Cell >Child 1</DataTable.Cell>
                         <DataTable.Cell numeric>c1</DataTable.Cell>
                         <DataTable.Cell numeric>c2</DataTable.Cell>
-                        <DataTable.Cell numeric>c3</DataTable.Cell>
                     </DataTable.Row>
                     <DataTable.Row key={0.2}>
                         <DataTable.Cell >Child 2</DataTable.Cell>
                         <DataTable.Cell numeric>c1</DataTable.Cell>
                         <DataTable.Cell numeric>c2</DataTable.Cell>
-                        <DataTable.Cell numeric>c3</DataTable.Cell>
                     </DataTable.Row>
                 </DataTable.Accordion>
                 <DataTable.Accordion 
@@ -91,25 +87,21 @@ export default function AccountScreen() {
                         <DataTable.Cell >Main2</DataTable.Cell>
                         <DataTable.Cell numeric>1</DataTable.Cell>
                         <DataTable.Cell numeric>2</DataTable.Cell>
-                        <DataTable.Cell numeric>3</DataTable.Cell>
                     </DataTable.Row>
                 }>
                     <DataTable.Row key={1.1}>
                         <DataTable.Cell >Child 1</DataTable.Cell>
                         <DataTable.Cell numeric>c1</DataTable.Cell>
                         <DataTable.Cell numeric>c2</DataTable.Cell>
-                        <DataTable.Cell numeric>c3</DataTable.Cell>
                     </DataTable.Row>
                     <DataTable.Row key={1.2}>
                         <DataTable.Cell >Child 2</DataTable.Cell>
                         <DataTable.Cell numeric>c1</DataTable.Cell>
                         <DataTable.Cell numeric>c2</DataTable.Cell>
-                        <DataTable.Cell numeric>c3</DataTable.Cell>
                     </DataTable.Row>
                 </DataTable.Accordion>
                 <DataTable.Row key={2} style={{paddingLeft:40}}>
                     <DataTable.MultiRowCell mainText="GME" subText="+100" subDirection={1}/>
-                    <DataTable.MultiRowCell numeric mainText="GME" subText="+10" subDirection={1}/>
                     <DataTable.MultiRowCell numeric mainText="GME" subText="+10" subDirection={1}/>
                     <DataTable.MultiRowCell numeric mainText="$10000" subText="13" subDirection={1}/>
                 </DataTable.Row>
@@ -117,85 +109,71 @@ export default function AccountScreen() {
                     <DataTable.Cell >Test 3</DataTable.Cell>
                     <DataTable.Cell numeric>1</DataTable.Cell>
                     <DataTable.Cell numeric>2</DataTable.Cell>
-                    <DataTable.Cell numeric>3</DataTable.Cell>
                 </DataTable.Row>
                 <DataTable.Row key={4}>
                     <DataTable.Cell >Test 4</DataTable.Cell>
                     <DataTable.Cell numeric>1</DataTable.Cell>
                     <DataTable.Cell numeric>2</DataTable.Cell>
-                    <DataTable.Cell numeric>3</DataTable.Cell>
                 </DataTable.Row>
                 <DataTable.Row key={5}>
                     <DataTable.Cell >Test 5</DataTable.Cell>
                     <DataTable.Cell numeric>1</DataTable.Cell>
                     <DataTable.Cell numeric>2</DataTable.Cell>
-                    <DataTable.Cell numeric>3</DataTable.Cell>
                 </DataTable.Row>
                 <DataTable.Row key={6}>
                     <DataTable.Cell >Test 6</DataTable.Cell>
                     <DataTable.Cell numeric>1</DataTable.Cell>
                     <DataTable.Cell numeric>2</DataTable.Cell>
-                    <DataTable.Cell numeric>3</DataTable.Cell>
                 </DataTable.Row>
                 <DataTable.Row key={7}>
                     <DataTable.Cell >Test 7</DataTable.Cell>
                     <DataTable.Cell numeric>1</DataTable.Cell>
                     <DataTable.Cell numeric>2</DataTable.Cell>
-                    <DataTable.Cell numeric>3</DataTable.Cell>
                 </DataTable.Row>
                 <DataTable.Row key={8}>
                     <DataTable.Cell >Test 8</DataTable.Cell>
                     <DataTable.Cell numeric>1</DataTable.Cell>
                     <DataTable.Cell numeric>2</DataTable.Cell>
-                    <DataTable.Cell numeric>3</DataTable.Cell>
                 </DataTable.Row>
                 <DataTable.Row key={9}>
                     <DataTable.Cell >Test 9</DataTable.Cell>
                     <DataTable.Cell numeric>1</DataTable.Cell>
                     <DataTable.Cell numeric>2</DataTable.Cell>
-                    <DataTable.Cell numeric>3</DataTable.Cell>
                 </DataTable.Row>
                 <DataTable.Row key={10}>
                     <DataTable.Cell >Test 10</DataTable.Cell>
                     <DataTable.Cell numeric>1</DataTable.Cell>
                     <DataTable.Cell numeric>2</DataTable.Cell>
-                    <DataTable.Cell numeric>3</DataTable.Cell>
                 </DataTable.Row>
                 <DataTable.Row key={11}>
                     <DataTable.Cell >Test 11</DataTable.Cell>
                     <DataTable.Cell numeric>1</DataTable.Cell>
                     <DataTable.Cell numeric>2</DataTable.Cell>
-                    <DataTable.Cell numeric>3</DataTable.Cell>
                 </DataTable.Row>
                 <DataTable.Row key={12}>
                     <DataTable.Cell >Test 12</DataTable.Cell>
                     <DataTable.Cell numeric>1</DataTable.Cell>
                     <DataTable.Cell numeric>2</DataTable.Cell>
-                    <DataTable.Cell numeric>3</DataTable.Cell>
                 </DataTable.Row>
                 <DataTable.Row key={13}>
                     <DataTable.Cell >Test 13</DataTable.Cell>
                     <DataTable.Cell numeric>1</DataTable.Cell>
                     <DataTable.Cell numeric>2</DataTable.Cell>
-                    <DataTable.Cell numeric>3</DataTable.Cell>
                 </DataTable.Row>
                 <DataTable.Row key={14}>
                     <DataTable.Cell >Test 14</DataTable.Cell>
                     <DataTable.Cell numeric>1</DataTable.Cell>
                     <DataTable.Cell numeric>2</DataTable.Cell>
-                    <DataTable.Cell numeric>3</DataTable.Cell>
                 </DataTable.Row>
                 <DataTable.Row key={15}>
                     <DataTable.Cell >Test 15</DataTable.Cell>
                     <DataTable.Cell numeric>1</DataTable.Cell>
                     <DataTable.Cell numeric>2</DataTable.Cell>
-                    <DataTable.Cell numeric>3</DataTable.Cell>
                 </DataTable.Row>
                 <DataTable.Row key={16}>
                     <DataTable.Cell >Test 16</DataTable.Cell>
                     <DataTable.Cell numeric>1</DataTable.Cell>
                     <DataTable.Cell numeric>2</DataTable.Cell>
-                    <DataTable.Cell numeric>3</DataTable.Cell>
                 </DataTable.Row>
             </DataTable>
             </ScrollView>
