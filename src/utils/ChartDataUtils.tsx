3883ef71c4fc7d91e6063ed6483cc8a4a21e6396
Alex Lee <alexlee500@gmail.com>
2021-05-21 02:27:18 -0700

    types

--- src/utils/ChartDataUtils.tsx
+++ src/utils/ChartDataUtils.tsx
@@ -1,4 +1,4 @@
-export const getDirection = (val) => {
+export const getDirection = (val:number) => {
     if (val > 0) return 1;
     if (val < 0) return -1;
     return 0;
@@ -8,12 +8,12 @@ export const percentDelta = (initial:number, final:number, places:number=2):numb
     return Number(((final - initial) / (Math.abs(initial)) * 100).toFixed(places))
 }
 
-export const percentToString = (val) => {
+export const percentToString = (val:number) => {
     return (!isNaN(val) && val != null) ? (
         (val>=0? '+':'') + val + '%'):('-')
 }
 
-export const valueToString = (val) => {
+export const valueToString = (val:number) => {
     return (!isNaN(val) && val != null)? 
     ((val < 0? '-' : '') + '$'+ Math.abs(val).toFixed(2)):( '-' )
 }
