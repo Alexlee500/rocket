00780c8f3f7d6a472a984a46f7bb4cf41f89c675
Alex Lee <alexlee500@gmail.com>
2021-05-24 02:28:26 -0700

    Update ChartDataUtils.tsx

--- src/utils/ChartDataUtils.tsx
+++ src/utils/ChartDataUtils.tsx
@@ -22,7 +22,4 @@ export const valueToString = (val) => {
 
 export const formatNumberString = (val) => val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 
-export const formatOptionTitle = (option, underlying) => {
-    console.log(`${option} ${underlying}`)
-    return option.replace(underlying+' ', '') 
-}
+export const formatOptionTitle = (option, underlying) => option.replace(underlying+' ', '')
